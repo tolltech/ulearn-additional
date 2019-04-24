f8837568082f209ea85629582945387e1b6677ad
tolltech <alexandrovpe@gmail.com>
2019-04-24 17:28:10 +0400

    proxy: clear program.cs

--- proxy/Task/Program.cs
+++ proxy/Task/Program.cs
@@ -1,36 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using SexyProxy;
-using Task.DontChange;
-
-namespace Task
+﻿namespace Task
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            var lazyBones = Proxy.CreateProxy<Lazybones>(invocation =>
-            {
-                var stopwatch = new Stopwatch();
-
-                stopwatch.Start();
-                var result = invocation.Proceed();
-                stopwatch.Stop();
-
-                Console.WriteLine(result.Result);
-
-                var method = invocation.Method;
-                var arguments = invocation.Arguments;
-                var spentMilliseconds = (int) stopwatch.ElapsedMilliseconds;
-                Tracer.LogTrace(
-                    $"{method?.DeclaringType?.Name} {method?.Name} {arguments.FirstOrDefault()} milliseconds",
-                    spentMilliseconds);
-                return result;
-            });
-
-            var task = new Zoo(new[] {lazyBones});
-            task.SpendTime();
         }
     }
 }
