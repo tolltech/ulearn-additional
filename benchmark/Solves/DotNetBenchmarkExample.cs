544ece5c083da025fc68049b57871fa2e1f0dabc
dubovtsev <dubovtsevskbkontur.ru>
2021-03-29 15:05:30 +0400

    Презентация и ее примеры

---
+++ benchmark/Solves/DotNetBenchmarkExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Running;
+using NUnit.Framework;
+
+namespace Solves
+{
+    [Config(typeof(ExampleConfig))]
+    public class DotNetBenchmarkExample
+    {
+        private class ExampleConfig : ManualConfig
+        {
+            public ExampleConfig()
+            {                
+                Add(StatisticColumn.Max); // Добавляем необходимую колонку 
+                Add(RPlotExporter.Default, CsvExporter.Default);
+            }
+        }
+        
+        [Params(10, 100, 1000, 10000)]
+        public int count;
+        
+        [Benchmark(Description = "Sum")]
+        public int TestSum()
+        {
+            return Enumerable.Range(1, count).Sum();
+        }
+    }
+
+    [TestFixture]
+    class TestBenchmark
+    {
+        [Test]
+        public void Test()
+        {
+            BenchmarkRunner.Run<DotNetBenchmarkExample>();
+        }
+    }
+}
---
+++ benchmark/Solves/ManualBenchmarkExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Solves
+{
+    public class ManualBenchmarkExample
+    {
+        //1. Запускаем Release mode without debugging
+        public void Run()
+        {
+            //2. Прогрев кеша процессора
+            for (int i = 0; i < 5; i++)
+            {
+                TestSum();
+            }
+            //3. Запуск на одном процессоре
+            Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+            //4. Stopwatch, а не DateTime
+            var stopwatch = new Stopwatch();
+            //5. Высокий приоритет приложения
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            //6. Сборка мусора
+            GC.Collect();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            stopwatch.Start();
+            TestSum(); // Можно сделать много прогонов и получить среднее
+            stopwatch.Stop();
+            Console.WriteLine($"{nameof(TestSum)}\t{stopwatch.ElapsedTicks} ticks");
+        }
+        
+        public int TestSum()
+        {
+            return Enumerable.Range(1, 10000).Sum();
+        }
+    }
+
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void METHOD()
+        {
+            var manualBenchmarkExample = new ManualBenchmarkExample();
+            manualBenchmarkExample.Run();
+        }
+    }
+}
